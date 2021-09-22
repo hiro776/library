2c8e63a5053c32465957a914a8169603900a631f
Rohit M <63762981+hiro776@users.noreply.github.com>
2021-09-22 16:25:14 +0530

    Add new book form working along with all other features, ie remove book, toogle read status, display available books

--- App.js
+++ App.js
@@ -24,6 +24,12 @@ function Book(title, author, pageCount, readStatus) {
     this.info = function () {
         return title + ', ' + author + ', ' + pageCount + ', ' + readStatus;
     }
+    this.toggleRead = function () {
+        if (this.readStatus)
+            this.readStatus = false;
+        else
+            this.readStatus = true;
+    }
 }
 
 const addBookToLibrary = (book) => {
@@ -31,15 +37,38 @@ const addBookToLibrary = (book) => {
         Library.push(book);
 }
 
+const closeForm = () => form.parentNode.style.visibility = 'hidden';
+
 addBookBtn.addEventListener('click', () => {
     form.parentNode.style.visibility = 'visible';
 });
-cancelBtn.onclick = () => form.parentNode.style.visibility = 'hidden';
 
-form.addEventListener("submit", (event) => {
-    // form.parentNode.style.visibility = 'hidden';
+cancelBtn.addEventListener('click', closeForm);
+window.addEventListener('keyup', e => {
+    // console.log(e.key);
+    if (e.key === 'Escape') {
+        closeForm();
+    }
+});
 
-    console.log(form.name);
+const clearForm = () => {
+    form.title.value = null;
+    form.author.value = null;
+    form.pgcount.value = null;
+    form.read.value = null;
+}
+
+form.addEventListener("submit", e => {
+    const read = (form.read.value === 'true') ? true : false;
+    const newBook = new Book(form.title.value, form.author.value, form.pgcount.value, read);
+    addBookToLibrary(newBook);
+
+    updateLibraryDisplay();
+
+    clearForm();
+    closeForm();
+
+    e.preventDefault();
 });
 
 
@@ -49,13 +78,6 @@ const removeBook = i => {
     Library.splice(i, 1);
 }
 
-// Toggle read status of the book at index i
-const toggleReadAt = i => {
-    if (Library[i].readStatus)
-        Library[i].readStatus = false;
-    else
-        Library[i].readStatus = true;
-}
 
 // Number -> DocumentElement
 // create a div element with class 'book' and data-id as the
@@ -95,12 +117,12 @@ const createBookElement = (i) => {
     const readDiv = document.createElement('div');
     readDiv.setAttribute('class', 'read');
     readDiv.setAttribute('title', 'Toggle Read Status');
-    readDiv.textContent = 'Read ' + ((Library[i].readStatus) ? '✅' : '❌');
+    readDiv.textContent = (Library[i].readStatus) ? 'Read ✅' : 'Not Read ❌';
 
     readDiv.addEventListener('click', () => {
         const index = readDiv.parentNode.getAttribute('data-id');
 
-        toggleReadAt(index);
+        Library[index].toggleRead();
 
         updateLibraryDisplay();
     });
@@ -128,19 +150,17 @@ const updateLibraryDisplay = () => {
 }
 
 // for testing:
-const book1 = new Book('Practical C Programming', 'Steve OUaline', 400, true);
-const book2 = new Book('A Complete Guide to C++ Programming', 'Ulla Kirch', 846, false);
-const book3 = new Book('Pro Git', 'Scott Chacon', 441, false);
-const book4 = new Book('Clean Code', 'Robert C. Martin', 462, false);
-const book5 = new Book('C Programming Tutorial', 'Tutorialspoint', 200, true);
-// console.log(cprogramming.constructor === Book);
+// const book1 = new Book('Practical C Programming', 'Steve OUaline', 400, true);
+// const book2 = new Book('A Complete Guide to C++ Programming', 'Ulla Kirch', 846, false);
+// const book3 = new Book('Pro Git', 'Scott Chacon', 441, false);
+// const book4 = new Book('Clean Code', 'Robert C. Martin', 462, false);
+// const book5 = new Book('C Programming Tutorial', 'Tutorialspoint', 200, true);
+// // console.log(cprogramming.constructor === Book);
 
-addBookToLibrary(book1);
-addBookToLibrary(book2);
-addBookToLibrary(book3);
-addBookToLibrary(book4);
-addBookToLibrary(book5);
-// ^^for testing.
+// addBookToLibrary(book1);
+// addBookToLibrary(book2);
+// addBookToLibrary(book3);
+// addBookToLibrary(book4);
+// addBookToLibrary(book5);
 
-console.log(Library);
-updateLibraryDisplay(Library);
+// updateLibraryDisplay(Library);
